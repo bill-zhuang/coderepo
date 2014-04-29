329a20627cbcc12cfe6ea574ef21d412e7090105
bill-zhuang <zstu_bill@yahoo.com>
2014-04-29 19:30:33 +0800

    + regex.js file

---
+++ public/js/regex.js
@@ -0,0 +1,46 @@
+
+function isNumber(n) 
+{
+    return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+function getTrimData(data)
+{
+    return data.replace(/^\s+|\s+$/g, '');
+}
+
+function isImageExtension(img_name)
+{
+	if (img_name.search(/\.(jpg|png|gif|jpeg|bmp)$/i) != -1) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function isCsvExtension(csv_name)
+{
+	if (csv_name.search(/\.csv$/i) != -1) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function isUnsignedInt(digit)
+{
+	if(digit.search(/^\s*[1-9][0-9]*\s*$/) != -1) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function isChineseFax(fax)
+{
+	if(fax.search(/^\s*([0-9]{3,4}-?[0-9]{7,8}(-[0-9]{3,4})?)\s*$/) != -1) {
+        return true;
+    } else {
+        return false;
+    }
+}
