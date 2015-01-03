9547292160c68ade6ba42f2bcf967bac9dc44754
bill-zhuang <zstu_bill@yahoo.com>
2015-01-03 11:30:39 +0800

    + fix bug of js get curent date  (add 0 to month & day less than 10, for checkDate validation case)

--- public/js/util.js
+++ public/js/util.js
@@ -61,6 +61,13 @@ function getCurrentDate()
     var month = d.getMonth() + 1;
     var day = d.getDate();
 
+    if (month < 10) {
+        month = '0' + month;
+    }
+    if (day < 10) {
+        day = '0' + day;
+    }
+
     return year + '-' + month + '-' + day;
 }
 
