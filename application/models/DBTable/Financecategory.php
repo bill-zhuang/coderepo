f812c8f081cd6cd05ca56f5eca78186a0839c1b9
bill-zhuang <zstu_bill@yahoo.com>
2015-01-17 16:38:50 +0800

    + finish finance category function

---
+++ application/models/DBTable/Financecategory.php
@@ -0,0 +1,64 @@
+<?php
+
+class Application_Model_DbTable_Financecategory extends Application_Model_DBTableFactory
+{
+    public function __construct()
+    {
+        parent::__construct('finance_category');
+    }
+
+    public function getFinancecategoryCount(array $conditions)
+    {
+        $select = $this->select()->reset()
+            ->from($this->_name, 'count(*) as total');
+        foreach ($conditions as $key => $content)
+        {
+            $select->where($key . ' ' . $content['compare_type'], $content['value']);
+        }
+        $count = $select->query()->fetchAll();
+        return $count[0]['total'];
+    }
+
+    public function getFinancecategoryData(array $conditions, $count, $offset, $order_by)
+    {
+        $select = $this->select()->reset();
+        foreach ($conditions as $key => $content)
+        {
+            $select->where($key . ' ' . $content['compare_type'], $content['value']);
+        }
+        $data = $select->limit($count, $offset)->order($order_by)
+            ->query()->fetchAll();
+        return $data;
+    }
+
+    public function getFinancecategoryByID($fc_id)
+    {
+        return $this->select()->reset()
+            ->where('fc_id=?', $fc_id)
+            ->query()->fetch();
+    }
+
+    public function getAllParentCategory()
+    {
+        $parent_data = $this->select()->reset()
+            ->from($this->_name, ['fc_id', 'fc_name'])
+            ->where('fc_parent_id=?', 0)->where('fc_status=?', 1)
+            ->query()->fetchAll();
+        $data = [];
+        foreach ($parent_data as $parent_value)
+        {
+            $data[$parent_value['fc_id']] = $parent_value['fc_name'];
+        }
+
+        return $data;
+    }
+
+    public function isFinanceCategoryExist($name, $fc_id)
+    {
+        $data = $this->select()->reset()
+            ->from($this->_name, 'count(*) as total')
+            ->where('fc_name=?', $name)->where('fc_id!=?', $fc_id)->where('fc_status=?', 1)
+            ->query()->fetchAll();
+        return $data[0]['total'] == 0 ? false : true;
+    }
+}
---
+++ application/modules/person/controllers/FinancecategoryController.php
@@ -0,0 +1,193 @@
+<?php
+
+class person_FinancecategoryController extends Zend_Controller_Action
+{
+    /**
+     * @var Application_Model_DBTable_financecategory
+     */
+    private $_adapter_finance_category;
+
+    public function init()
+    {
+        /* Initialize action controller here */
+        $this->_helper->layout()->setLayout('layout');
+        $this->_adapter_finance_category = new Application_Model_DBTable_financecategory();
+        $this->view->assign(
+            [
+                'parents' => $this->_adapter_finance_category->getAllParentCategory(),
+            ]
+        );
+    }
+
+    public function indexAction()
+    {
+        // action body
+        $current_page = intval($this->_getParam('current_page', Bootstrap::INIT_START_PAGE));
+        $page_length = intval($this->_getParam('page_length', Bootstrap::INIT_PAGE_LENGTH));
+        $start = ($current_page - Bootstrap::INIT_START_PAGE) * $page_length;
+        $keyword = trim($this->_getParam('keyword', ''));
+
+        $conditions = [
+            'fc_status' => [
+                'compare_type' => '= ?',
+                'value' => Bootstrap::VALID_STATUS
+            ]
+        ];
+        if ('' !== $keyword)
+        {
+            $conditions['fc_name'] = [
+                'compare_type' => 'like ?',
+                'value' => '%' . $keyword . '%'
+            ];
+        }
+        $order_by = 'fc_update_time desc';
+        $total = $this->_adapter_finance_category->getFinancecategoryCount($conditions);
+        $data = $this->_adapter_finance_category->getFinancecategoryData($conditions, $page_length, $start, $order_by);
+
+        $view_data = [
+            'data' => $data,
+            'current_page' => $current_page,
+            'page_length' => $page_length,
+            'total_pages' => ceil($total / $page_length) ? ceil($total / $page_length) : Bootstrap::INIT_TOTAL_PAGE,
+            'total' => $total,
+            'start' => $start,
+            'keyword' => $keyword,
+        ];
+        $this->view->assign($view_data);
+    }
+
+    public function addfinancecategoryAction()
+    {
+        $affected_rows = Bootstrap::INIT_AFFECTED_ROWS;
+        if (isset($_POST['add_financecategory_name']))
+        {
+            try 
+            {
+                $this->_adapter_finance_category->getAdapter()->beginTransaction();
+                $affected_rows = $this->_addFinancecategory();
+                $this->_adapter_finance_category->getAdapter()->commit();
+            }
+            catch (Exception $e)
+            {
+                $affected_rows = Bootstrap::INIT_AFFECTED_ROWS;
+                $this->_adapter_finance_category->getAdapter()->rollBack();
+            }
+        }
+        
+        echo json_encode($affected_rows);
+        exit;
+    }
+    
+    public function modifyfinancecategoryAction()
+    {
+        $affected_rows = Bootstrap::INIT_AFFECTED_ROWS;
+        if (isset($_POST['modify_financecategory_fc_id']))
+        {
+            try
+            {
+                $this->_adapter_finance_category->getAdapter()->beginTransaction();
+                $affected_rows = $this->_updateFinancecategory();
+                $this->_adapter_finance_category->getAdapter()->commit();
+            }
+            catch (Exception $e)
+            {
+                $affected_rows = Bootstrap::INIT_AFFECTED_ROWS;
+                $this->_adapter_finance_category->getAdapter()->rollBack();
+            }
+        }
+        
+        echo json_encode($affected_rows);
+        exit;
+    }
+    
+    public function deletefinancecategoryAction()
+    {
+        $affected_rows = Bootstrap::INIT_AFFECTED_ROWS;
+        if (isset($_POST['fc_id']))
+        {
+            try
+            {
+                $this->_adapter_finance_category->getAdapter()->beginTransaction();
+                $fc_id = intval($_POST['fc_id']);
+                $update_data = [
+                    'fc_status' => Bootstrap::INVALID_STATUS,
+                    'fc_update_time' => date('Y-m-d H:i:s')
+                ];
+                $where = $this->_adapter_finance_category->getAdapter()
+                    ->quoteInto('status=1 and (fc_id=? or fc_parent_id=?)', $fc_id);
+                $affected_rows = $this->_adapter_finance_category->update($update_data, $where);
+                $this->_adapter_finance_category->getAdapter()->commit();
+            }
+            catch (Exception $e)
+            {
+                $affected_rows = Bootstrap::INIT_AFFECTED_ROWS;
+                $this->_adapter_finance_category->getAdapter()->rollBack();
+            }
+        }
+        
+        echo json_encode($affected_rows);
+        exit;
+    }
+    
+    public function getfinancecategoryAction()
+    {
+        $data = [];
+        if (isset($_POST['fc_id']))
+        {
+            $fc_id = intval($_POST['fc_id']);
+            if ($fc_id > Bootstrap::INVALID_PRIMARY_ID)
+            {
+                $data = $this->_adapter_finance_category->getFinancecategoryByID($fc_id);
+            }
+        }
+
+        echo json_encode($data);
+        exit;
+    }
+    
+    private function _addFinancecategory()
+    {
+        $affected_rows = 0;
+
+        $name = trim($_POST['add_financecategory_name']);
+        $parent_id = intval($_POST['add_financecategory_parent_id']);
+        $add_time = date('Y-m-d H:i:s');
+
+        if (!$this->_adapter_finance_category->isFinanceCategoryExist($name, 0))
+        {
+            $data = [
+                'fc_name' => $name,
+                'fc_parent_id' => $parent_id,
+                'fc_status' => Bootstrap::VALID_STATUS,
+                'fc_create_time' => $add_time,
+                'fc_update_time' => $add_time
+            ];
+            $affected_rows = $this->_adapter_finance_category->insert($data);
+        }
+
+        return $affected_rows;
+    }
+    
+    private function _updateFinancecategory()
+    {
+        $affected_rows = 0;
+
+        $fc_id = intval($_POST['modify_financecategory_fc_id']);
+        $name = trim($_POST['modify_financecategory_name']);
+        $parent_id = intval($_POST['modify_financecategory_parent_id']);
+
+        if (!$this->_adapter_finance_category->isFinanceCategoryExist($name, $fc_id))
+        {
+            $data = [
+                'fc_name' => $name,
+                'fc_parent_id' => $parent_id,
+                'fc_update_time' => date('Y-m-d H:i:s')
+            ];
+            $where = $this->_adapter_finance_category->getAdapter()->quoteInto('fc_id=?', $fc_id);
+            $affected_rows = $this->_adapter_finance_category->update($data, $where);
+        }
+
+        return $affected_rows;
+    }
+
+}
