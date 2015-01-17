84529b0bf67ef06ffee0bfb11da56eb55d0b8cd5
bill-zhuang <zstu_bill@yahoo.com>
2015-01-17 16:39:28 +0800

    + init total page in bootstrap

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -4,6 +4,7 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
     const INIT_START_PAGE = 1;
     const INIT_PAGE_LENGTH = 25;
+    const INIT_TOTAL_PAGE = 1;
 
     const VALID_STATUS = 1;
     const INVALID_STATUS = 0;
