e1719a342af7dbc17199e22408f58e8d766ac851
bill-zhuang <zstu_bill@yahoo.com>
2015-07-23 13:59:26 +0800

    + extrach color hex js to js file

---
+++ public/js/color-hex.js
@@ -0,0 +1,6 @@
+$('#farbtastic_color').val('#123456');
+$('#farbtastic_colorpicker').farbtastic('#farbtastic_color');
+//or $.farbtastic('#farbtastic_colorpicker', '#farbtastic_color');
+//
+$.minicolors.defaults.control = 'wheel';
+$('#mini_color').minicolors('create');
