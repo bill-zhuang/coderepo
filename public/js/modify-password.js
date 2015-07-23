61ab95ea9175e4b85dd06b8a3639216b2b2facb5
bill-zhuang <zstu_bill@yahoo.com>
2015-07-23 14:05:18 +0800

    + extract modify password js

---
+++ public/js/modify-password.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $('#old_password').val('');
+});
+$('#modifypasswordform').on('submit', function(event){
+    event.preventDefault();
+    modifyPassword();
+});
+
+function modifyPassword()
+{
+    var old_password = $.trim($('#old_password').val());
+    var new_password = $.trim($('#new_password').val());
+    var new_password_repeat = $.trim($('#new_password_repeat').val());
+
+    if (old_password == '') {
+        alert('密码不能为空！');
+    } else if (new_password == '') {
+        alert('新密码不能为空！');
+    } else if (new_password_repeat == '') {
+        alert('新密码确认不能为空！');
+    } else if (new_password != new_password_repeat) {
+        alert('两次密码不相同');
+    } else {
+        $('#modifypasswordform').get(0).submit();
+    }
+}
