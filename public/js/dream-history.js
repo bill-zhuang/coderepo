b6eff4daf7bff6b3bd8284c09803800048d79b3b
bill-zhuang <zstu_bill@yahoo.com>
2015-07-24 11:16:36 +0800

    + extract pagination js code to file & update corresponding view & js file

--- public/js/dream-history.js
+++ public/js/dream-history.js
@@ -40,34 +40,6 @@ function ajaxIndex() {
     callAjaxWithFunction(get_url, get_data, success_function, method);
 }
 /*  --------------------------------------------------------------------------------------------------------  */
-function initPagination(total_pages, current_page) {
-    $('#div_pagination').empty().append('<ul id="pagination" class="pagination-md"></ul>');
-    $('#pagination').twbsPagination({
-        totalPages: total_pages,
-        startPage: current_page,
-        visiblePages: 7,
-        first: '首页',
-        prev: '上一页',
-        next: '下一页',
-        last: '尾页',
-        onPageClick: function (event, page) {
-            $('#current_page').val(page);
-            ajaxIndex();
-        }
-    });
-}
-
-$('#page_length').on('change', function() {
-    $('#current_page').val(1);
-    ajaxIndex();
-});
-
-$('#btn_search').on('click', function(event) {
-    event.preventDefault();
-    $('#current_page').val(1);
-    ajaxIndex();
-});
-/*  --------------------------------------------------------------------------------------------------------  */
 $('#btn_add').on('click', function() {
     window.DreamHistoryForm.reset();
     $('#dream_history_date').val(getCurrentDate());
--- public/js/finance-payment.js
+++ public/js/finance-payment.js
@@ -41,34 +41,6 @@ function ajaxIndex() {
     callAjaxWithFunction(get_url, get_data, success_function, method);
 }
 /*  --------------------------------------------------------------------------------------------------------  */
-function initPagination(total_pages, current_page) {
-    $('#div_pagination').empty().append('<ul id="pagination" class="pagination-md"></ul>');
-    $('#pagination').twbsPagination({
-        totalPages: total_pages,
-        startPage: current_page,
-        visiblePages: 7,
-        first: '首页',
-        prev: '上一页',
-        next: '下一页',
-        last: '尾页',
-        onPageClick: function (event, page) {
-            $('#current_page').val(page);
-            ajaxIndex();
-        }
-    });
-}
-
-$('#page_length').on('change', function() {
-    $('#current_page').val(1);
-    ajaxIndex();
-});
-
-$('#btn_search').on('click', function(event) {
-    event.preventDefault();
-    $('#current_page').val(1);
-    ajaxIndex();
-});
-/*  --------------------------------------------------------------------------------------------------------  */
 var g_selectpicker = $('#finance_payment_fc_id').selectpicker();
 
 $('#btn_add').on('click', function() {
