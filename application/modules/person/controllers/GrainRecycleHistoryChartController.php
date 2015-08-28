04a73d3d9af903044cf6891f52eac34642643f45
bill-zhuang <zstu_bill@yahoo.com>
2015-08-28 13:53:17 +0800

    + divided grain recycle history chart of period & month to seperately

--- application/modules/person/controllers/GrainRecycleHistoryChartController.php
+++ application/modules/person/controllers/GrainRecycleHistoryChartController.php
@@ -19,16 +19,20 @@ class person_GrainRecycleHistoryChartController extends Zend_Controller_Action
         // action body
     }
 
-    public function ajaxIndexAction()
-    {
-        echo json_encode($this->_index());
-        exit;
-    }
-
     public function ajaxGrainRecycleHistoryPeriodAction()
     {
         list($start_date, $end_date) = $this->_getSearchParams();
-        echo json_encode($this->_getAllGrainRecycleHistoryDataByDay($start_date, $end_date));
+        $data = $this->_getAllGrainRecycleHistoryDataByDay($start_date, $end_date);
+
+        echo json_encode($data);
+        exit;
+    }
+
+    public function ajaxGrainRecycleHistoryMonthAction()
+    {
+        $data = $this->_getAllDreamHistoryDataByMonth();
+
+        echo json_encode($data);
         exit;
     }
 
@@ -53,30 +57,6 @@ class person_GrainRecycleHistoryChartController extends Zend_Controller_Action
         exit;
     }
 
-    private function _index()
-    {
-        $chart_data = [
-            'period' => [],
-            'number' => [],
-        ];
-        $month_data = $this->_adapter_grain_recycle_history->getTotalGrainRecycleHistoryGroupData();
-        foreach ($month_data as $month_value)
-        {
-            $chart_data['period'][] = $month_value['period'];
-            $chart_data['number'][] = $month_value['number'];
-        }
-
-        list($start_date, $end_date) = $this->_getSearchParams();
-        $all_chart_data = $this->_getAllGrainRecycleHistoryDataByDay($start_date, $end_date);
-
-        return [
-            'start_date' => $start_date,
-            'end_date' => $end_date,
-            'chart_data' => $chart_data,
-            'all_chart_data' => $all_chart_data,
-        ];
-    }
-
     private function _getAllGrainRecycleHistoryDataByDay($start_date, $end_date)
     {
         $all_chart_data = [
@@ -93,6 +73,22 @@ class person_GrainRecycleHistoryChartController extends Zend_Controller_Action
         }
 
         return $all_chart_data;
+    }
+
+    private function _getAllDreamHistoryDataByMonth()
+    {
+        $data = [
+            'period' => [],
+            'number' => [],
+        ];
+        $month_data = $this->_adapter_grain_recycle_history->getTotalGrainRecycleHistoryGroupData();
+        foreach ($month_data as $month_value)
+        {
+            $data['period'][] = $month_value['period'];
+            $data['number'][] = $month_value['number'];
+        }
+
+        return $data;
     }
 
     private function _getSearchParams()
--- public/js/grain-recycle-history-chart.js
+++ public/js/grain-recycle-history-chart.js
@@ -1,24 +1,32 @@
 $(document).ready(function () {
-    //all grain recycle history data by day
-    initChart();
+    initPeriodChart();
+    initMonthChart();
 });
 
-function initChart() {
-    var get_url = '/person/grain-recycle-history-chart/ajax-index';
-    var get_data = {
-
-    };
+function initPeriodChart() {
+    var get_url = '/person/grain-recycle-history-chart/ajax-grain-recycle-history-period';
+    var get_data = $.param($('#formSearch').serializeArray());
     var method = 'get';
     var success_function = function (result) {
-        var chart_data = result.chart_data;
-        var data_period = chart_data['period'];
-        var data_number = chart_data['number'];
-        if (result.start_date != '') {
-            $('#start_date').val(result.start_date);
-        }
-        if (result.end_date != '') {
-            $('#start_date').val(result.end_date);
-        }
+        var line_option = {
+            responsive: true,
+            scaleOverride: true,
+            scaleSteps: 5, //y axis length = steps * step_width
+            scaleStepWidth: 1, // y axis
+            scaleStartValue: 0 // y axis start value
+        };
+        initLineChart('grain_recycle_history_line_chart_all', result['period'], result['interval'], line_option);
+    };
+    callAjaxWithFunction(get_url, get_data, success_function, method);
+}
+
+function initMonthChart() {
+    var get_url = '/person/grain-recycle-history-chart/ajax-grain-recycle-history-month';
+    var get_data = $.param($('#formSearch').serializeArray());
+    var method = 'get';
+    var success_function = function (result) {
+        var data_period = result['period'];
+        var data_number = result['number'];
         var line_option = {
             responsive: true,
             scaleOverride: true,
@@ -28,30 +36,11 @@ function initChart() {
         };
         initLineChart('grain_recycle_history_line_chart', data_period, data_number, line_option);
         initBarChart('grain_recycle_history_bar_chart', data_period, data_number);
-
-        line_option = {
-            responsive: true,
-            scaleOverride: true,
-            scaleSteps: 5, //y axis length = steps * step_width
-            scaleStepWidth: 1, // y axis
-            scaleStartValue: 0 // y axis start value
-        };
-        initLineChart('grain_recycle_history_line_chart_all', result.all_chart_data['period'], result.all_chart_data['interval'], line_option);
     };
     callAjaxWithFunction(get_url, get_data, success_function, method);
 }
 
 $('#btn_search').on('click', function (event) {
     event.preventDefault();
-    ajaxDreamHistoryPeriod();
+    initPeriodChart();
 });
-
-function ajaxDreamHistoryPeriod() {
-    var get_url = '/person/grain-recycle-history-chart/ajax-grain-recycle-history-period';
-    var get_data = $.param($('#formSearch').serializeArray());
-    var method = 'get';
-    var success_function = function (result) {
-        initLineChart('grain_recycle_history_line_chart_all', result['period'], result['interval']);
-    };
-    callAjaxWithFunction(get_url, get_data, success_function, method);
-}
