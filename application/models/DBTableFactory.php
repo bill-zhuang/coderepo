1283ca6ca408beed7b641d03f749d74d2554246e
bill-zhuang <zstu_bill@yahoo.com>
2016-03-03 14:10:04 +0800

    + using private variable instead

--- application/models/DBTableFactory.php
+++ application/models/DBTableFactory.php
@@ -15,7 +15,7 @@ class Application_Model_DBTableFactory extends Zend_Db_Table_Abstract
         $this->_table_name = $table_name;
         $config = [
             Zend_Db_Table_Abstract::ADAPTER => Application_Model_DBAdapter::getDBAdapter($section_name),
-            Zend_Db_Table_Abstract::NAME => $table_name,
+            Zend_Db_Table_Abstract::NAME => $this->_table_name,
         ];
         parent::__construct($config);
     }
