83f5d8ce7d9eeda0d31ffe82d7358b59625f27f0
bill-zhuang <zstu_bill@yahoo.com>
2016-03-18 17:14:05 +0800

    + scroll up css & js

--- public/js/layout.js
+++ public/js/layout.js
@@ -8,6 +8,18 @@ $(document).ready(function(){
             }
         });
     }
+    //scroll up
+    $(window).scroll(function() {
+        if($(this).scrollTop() > 100){
+            $('#go-top').stop().animate({
+                bottom: '25px'
+            }, 500);
+        } else {
+            $('#go-top').stop().animate({
+                bottom: '-100px'
+            }, 500);
+        }
+    });
 });
 
 $('ul.treeview-menu li a').on('click', function(){
@@ -17,4 +29,14 @@ $('ul.treeview-menu li a').on('click', function(){
     $(this).parent().parent().parent().addClass('active');
     var href = $(this).attr('href');
     $.cookie('nav-top', href, {expires : 1, path : '/'});
+});
+
+$('#go-top').click(function() {
+    $('html, body').stop().animate({
+        scrollTop: 0
+    }, 500, function() {
+        $('#go-top').stop().animate({
+            bottom: '-100px'
+        }, 500);
+    });
 });
