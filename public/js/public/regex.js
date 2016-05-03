bc46a0ebc1bffbaa1fbbe92725dd710bf933a9d7
bill-zhuang <zstu_bill@yahoo.com>
2016-05-03 14:26:57 +0800

    + update js regex code

--- public/js/public/regex.js
+++ public/js/public/regex.js
@@ -7,33 +7,17 @@ function getTrimData(data) {
 }
 
 function isImageExtension(img_name) {
-    if (img_name.search(/\.(jpg|png|gif|jpeg|bmp)$/i) != -1) {
-        return true;
-    } else {
-        return false;
-    }
+    return (img_name.search(/\.(jpg|png|gif|jpeg|bmp)$/i) != -1);
 }
 
 function isCsvExtension(csv_name) {
-    if (csv_name.search(/\.csv$/i) != -1) {
-        return true;
-    } else {
-        return false;
-    }
+    return (csv_name.search(/\.csv$/i) != -1);
 }
 
 function isUnsignedInt(digit) {
-    if (digit.search(/^\s*[1-9][0-9]*\s*$/) != -1) {
-        return true;
-    } else {
-        return false;
-    }
+    return (digit.search(/^\s*[1-9][0-9]*\s*$/) != -1);
 }
 
 function isChineseFax(fax) {
-    if (fax.search(/^\s*([0-9]{3,4}-?[0-9]{7,8}(-[0-9]{3,4})?)\s*$/) != -1) {
-        return true;
-    } else {
-        return false;
-    }
+    return (fax.search(/^\s*([0-9]{3,4}-?[0-9]{7,8}(-[0-9]{3,4})?)\s*$/) != -1);
 }
